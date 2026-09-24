778e6a6ac4ec010aa4c049386bfd412d20b984ae
Jaya Joshi <jaya@borekit.com>
2026-09-24 11:55:29 +0530

    scripts dir

---
+++ generated/typescript/contracts/index.ts
@@ -0,0 +1,24 @@
+export * from "./framework_object";
+export type {
+  LayoutId,
+  FrameworkReference,
+  PresentationPlan,
+  PlannedSlide,
+} from "./presentation_plan";
+export type { ChapterId, FieldProvenanceEntry, SlideSpecBase } from "./slide_spec_base";
+export type { Cover01SlideSpec } from "./slide_spec_group_a_cover_01";
+export type { Context01SlideSpec } from "./slide_spec_group_a_context_01";
+export type { ProblemSolution01SlideSpec } from "./slide_spec_group_a_problem_solution_01";
+export type { Scope01SlideSpec } from "./slide_spec_group_a_scope_01";
+export type { RequirementsMatrix01SlideSpec } from "./slide_spec_group_a_requirements_matrix_01";
+export type { ExecutiveSummary01SlideSpec } from "./slide_spec_summary_executive_summary_01";
+export type { ProcessFlow01SlideSpec } from "./slide_spec_group_b_process_flow_01";
+export type { Timeline01SlideSpec } from "./slide_spec_group_b_timeline_01";
+export type { Milestones01SlideSpec } from "./slide_spec_group_b_milestones_01";
+export type { TeamFte01SlideSpec } from "./slide_spec_group_b_team_fte_01";
+export type { Architecture01SlideSpec } from "./slide_spec_group_c_architecture_01";
+export type { Compliance01SlideSpec } from "./slide_spec_group_c_compliance_01";
+export type { SuccessMetrics01SlideSpec } from "./slide_spec_group_c_success_metrics_01";
+export type { OpenQuestions01SlideSpec } from "./slide_spec_group_c_open_questions_01";
+export type { NextSteps01SlideSpec } from "./slide_spec_group_c_next_steps_01";
+
